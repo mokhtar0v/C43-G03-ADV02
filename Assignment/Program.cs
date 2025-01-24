5a6f835afd4287615fd650cac18353b102bfc23d
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-24 18:02:44 +0200

    Question02

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,31 +5,50 @@
         static void Main(string[] args)
         {
             #region Question01
+            //    int N = int.Parse(Console.ReadLine());
+            //    List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //    arr.Sort();
+            //    int Q = int.Parse(Console.ReadLine());
+            //    for (int i = 0; i < Q; i++)
+            //    {
+            //        int X = int.Parse(Console.ReadLine());
+            //        int index = arr.BinarySearch(X);
+
+            //        if (index < 0)
+            //        {
+            //            index = ~index;
+            //        }
+            //        else
+            //        {
+            //            index++;
+            //            while (index < N && arr[index] == X)
+            //            {
+            //                index++;
+            //            }
+            //        }
+            //        int count = N - index;
+            //        Console.WriteLine(count);
+            //    }
+            //} 
+            #endregion
+
+            #region Question02
             int N = int.Parse(Console.ReadLine());
             List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
-            arr.Sort();
-            int Q = int.Parse(Console.ReadLine());
-            for (int i = 0; i < Q; i++)
+            bool Flag = true;
+            for (int i = 0; i < N / 2; i++)
             {
-                int X = int.Parse(Console.ReadLine());
-                int index = arr.BinarySearch(X);
-
-                if (index < 0)
+                if (arr[i] != arr[N - 1 - i])
                 {
-                    index = ~index;
+                    Flag = false;
+                    break;
                 }
-                else
-                {
-                    index++;
-                    while (index < N && arr[index] == X)
-                    {
-                        index++;
-                    }
-                }
-                int count = N - index;
-                Console.WriteLine(count);
             }
-        } 
-        #endregion
+            if (Flag) Console.WriteLine("YES");
+            else Console.WriteLine("NO"); 
+            #endregion
+
+
+        }
     }
 }
