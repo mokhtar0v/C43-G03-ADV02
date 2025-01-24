46c637d2ec0cd3c8e6854b489400aa98ae2502ff
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-24 17:56:01 +0200

    Question01

---
+++ Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question01
+            int N = int.Parse(Console.ReadLine());
+            List<int> arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+            arr.Sort();
+            int Q = int.Parse(Console.ReadLine());
+            for (int i = 0; i < Q; i++)
+            {
+                int X = int.Parse(Console.ReadLine());
+                int index = arr.BinarySearch(X);
+
+                if (index < 0)
+                {
+                    index = ~index;
+                }
+                else
+                {
+                    index++;
+                    while (index < N && arr[index] == X)
+                    {
+                        index++;
+                    }
+                }
+                int count = N - index;
+                Console.WriteLine(count);
+            }
+        } 
+        #endregion
+    }
+}
