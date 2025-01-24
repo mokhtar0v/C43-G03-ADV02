8e050116c14a236248dcf8272aeb74c5d3a694ad
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-24 17:45:33 +0200

    ArrayList

--- Session02 Advanced/Program.cs
+++ Session02 Advanced/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Session02_Advanced
+﻿using System.Collections;
+
+namespace Session02_Advanced
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region ArrayList
+            ///ArrayList nums = new ArrayList(5) { 1, 2, 3, 4, 5 };
+            ///Console.WriteLine($"Count = {nums.Count}, Capacity = {nums.Capacity}");
+            ///if (nums.Capacity < 21)
+            ///    nums.Capacity = 21;
+            ///for (int i = 6; i<=21;  i++)
+            ///{
+            ///    nums.Add(i);
+            ///}
+            ///Console.WriteLine($"Count = {nums.Count}, Capacity = {nums.Capacity}");
+            ///Adding and Trimming
+            ///nums.Add(1);
+            ///nums.Add(2);
+            ///nums.AddRange(new int[] { 3, 4 });
+            ///Console.WriteLine($"Count = {nums.Count}, Capacity = {nums.Capacity}");
+            ///nums.Add(5);
+            ///Console.WriteLine($"Count = {nums.Count}, Capacity = {nums.Capacity}");
+            ///nums.TrimToSize();
+            ///Console.WriteLine("After Trimming");
+            ///Console.WriteLine($"Count = {nums.Count}, Capacity = {nums.Capacity}");
+            #endregion
         }
     }
 }
